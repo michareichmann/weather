8e44db12e6998ff51eaf69e0867cfab39b1dcc87
michareichmann <micha.reichmann@gmail.com>
2023-06-13 11:58:59 +0200

    add Dir and Config

--- src/data.py
+++ src/data.py
@@ -2,13 +2,14 @@ from urllib import request
 import json
 from datetime import datetime
 from pathlib import Path
+from plotting.utils import Config, BaseDir
 
 class Data:
 
-    Dir: Path = ...
-    Config = ...
+    Dir: Path = BaseDir.joinpath('data')
+    Config = Config(BaseDir.joinpath('config', 'main.ini'), section='visual crossing')
 
-    Location = 'Wroclaw'
+    Location = Config.get_value('Location')
     Url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline'
 
     @staticmethod
@@ -24,5 +25,5 @@ class Data:
     def load(month: int = None, year: int = None) -> dict:
         now = datetime.now()
         now = now.replace(month=now.month if month is None else month, year=now.year if year is None else year)
-        with open(Data.Dir.joinpath(now.strftime('%Y-%b'.lower()))) as f:
+        with open(Data.Dir.joinpath(now.strftime('%Y-%b.json').lower())) as f:
             return json.load(f)
